64157fdf6db4c0a2fca7629688a17f352761b657
minh577nbb <67152363+minh577nbb@users.noreply.github.com>
2020-06-25 16:05:09 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/%C4%90%E1%BA%A5t-Xanh-Group-106716874369594/notifications/'>https://www.facebook.com/%C4%90%E1%BA%A5t-Xanh-Group-106716874369594</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.nhatminhbds.blogspot.com'>https://www.nhatminhbds.blogspot.com";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
